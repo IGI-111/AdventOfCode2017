fbcbaf05d496814a436a0dd594fff21fd3e3cbfc
IGI-111 <igi.222@gmail.com>
2017-12-11 14:37:57 +0000

    added comments to 11

--- 11.js
+++ 11.js
@@ -16,9 +16,12 @@ rl.on('line', line => {
   };
   const positions = line.split(',')
     .map(dir => directions[dir])
-    .map((dir, i, list) => list.slice(0, i + 1))
-    .map(dirs => dirs
-      .reduce((acc, dir) => [acc[0] + dir[0], acc[1] + dir[1], acc[2] + dir[2]], [0, 0, 0]))
-    .map(pos => pos.reduce((acc, coor) => Math.max(acc, coor), 0));
+    .map((dir, i, list) => list
+      // turn into arrays of all moves so far
+      .slice(0, i + 1)
+      // turn into all walked through positions
+      .reduce((acc, dir) => [acc[0] + dir[0], acc[1] + dir[1], acc[2] + dir[2]], [0, 0, 0])
+      // turn into all distances from origin
+      .reduce((acc, coor) => Math.max(acc, coor), 0));
   console.log(`${positions[positions.length - 1]} ${Math.max(...positions)}`);
 });
